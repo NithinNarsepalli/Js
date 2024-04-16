4dd4292f5ae7c8a84471e040ed32c91a0ce63922
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 22:08:56 +0530

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,30 @@
+//task2(answers are below)
+//--------------------------------------------------------
+//create variable name(firstFavNum)&store your favourite number
+//create variable name(secondFavNum) & store your second favourite number
+//add both numbers
+//subtract
+//multiply
+//divide
+//modulos
+//powerof (exponential)
+//--------------------------------------------------------
+
+//create variable name(firstFavNum)&store your favourite number
+let firstFavNum=7;
+console.log(typeof firstFavNum);
+//create variable name(secondFavNum) & store your second favourite number
+let secondFavNumber=8;
+console.log(typeof secondFavNumber);
+//add both numbers
+console.log(firstFavNum + secondFavNumber);
+//subtract
+console.log(firstFavNum - secondFavNumber);
+//multiply
+console.log(firstFavNum * secondFavNumber);
+//divide
+console.log(firstFavNum / secondFavNumber);
+//modulos
+console.log(firstFavNum % secondFavNumber);
+//powerof (exponential)
+console.log(firstFavNum ** secondFavNumber);
