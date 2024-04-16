eea6881d5d9778466e7b4c269f3e1b9bec08100e
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 20:48:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+//1.create variable name(name) & store your name
+var name="nithin";
+//2.create variable name (whatDoYouWannaBecomeInYourLife)&store "programmer"
+var whatDoYouWannaBecomeInYourLife ="programmer";
+//3.create variable name(gender)&store your gender
+var gender="male";
+//4.create variable name (twitterHandle)& stire your twitter handle.
+var twitterHandle="@Nithin11";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
