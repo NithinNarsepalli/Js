84ed5c83453d2c7e6603e02cbc8c002a62ff8398
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 22:35:50 +0530

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -28,3 +28,24 @@ console.log(firstFavNum / secondFavNumber);
 console.log(firstFavNum % secondFavNumber);
 //powerof (exponential)
 console.log(firstFavNum ** secondFavNumber);
+
+
+//task3 boolean datatypes (answers are below)
+//__________________________________________________
+//1.create variable name (isJsProgrammingLanguage) &store true as a value
+//2.create variable name(JsIsHard) &store false as a value
+//3.create variable name(favNumb)&store your favourite number inside
+//4.now add favnumber with the value of undefined
+//_____________________________________________
+
+
+//1.create variable name (isJsProgrammingLanguage) &store true as a value
+let isJsProgrammingLanguage=true;
+console.log(isJsProgrammingLanguage)
+//2.create variable name(JsIsHard) &store false as a value
+let JsIsHard=false;
+console.log(JsIsHard)
+//3.create variable name(favNumb)&store your favourite number inside
+let favNumb=7;
+//4.now add favnumber with the value of undefined
+console.log(favNumb + undefined);
