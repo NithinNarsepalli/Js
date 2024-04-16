2c737a52557ef817998bc53f222ca711a11da519
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 21:38:07 +0530

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+//number data type
+let num=7;//assigining the value to variable
+console.log(num);
+//to find the data type of the variabe
+console.log(typeof num);
+//do mathematics in js
+console.log(num+1);//addition
+console.log(num-1);//subtraction
+console.log(num*1);//myultiplication
+console.log(num/1);//division
+console.log(num**1);//exponential
+console.log(num%1);//modulo
