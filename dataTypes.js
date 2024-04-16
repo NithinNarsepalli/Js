d51f007e6f6f101aba8f6ddef6cfa0e625407e07
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 21:55:58 +0530

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -2,7 +2,7 @@
 let num=7;//assigining the value to variable
 console.log(num);
 //to find the data type of the variabe
-console.log(typeof num);
+console.log(typeof num); //"typeof" is the reserved keyword in js which is used to find the data type of the variable
 //do mathematics in js
 //addition
 console.log(num+1);
