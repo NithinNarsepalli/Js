034d07e8955589630a4c241af881e1c1f45eb026
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-16 21:43:06 +0530

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -4,9 +4,15 @@ console.log(num);
 //to find the data type of the variabe
 console.log(typeof num);
 //do mathematics in js
-console.log(num+1);//addition
-console.log(num-1);//subtraction
-console.log(num*1);//myultiplication
-console.log(num/1);//division
-console.log(num**1);//exponential
-console.log(num%1);//modulo
+//addition
+console.log(num+1);
+//subtraction
+console.log(num-1);
+//multiplication
+console.log(num*1);
+//division
+console.log(num/1);
+//exponential
+console.log(num**1);
+//modulo
+console.log(num%1);
