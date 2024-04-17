a526c0dd7a6e38037a550216a687741a66edc68a
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-17 20:50:50 +0530

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -49,3 +49,38 @@ console.log(JsIsHard)
 let favNumb=7;
 //4.now add favnumber with the value of undefined
 console.log(favNumb + undefined);
+
+---------------------------------------------------------------------------------------------------------
+//task4(answers are below)
+// 1. Create variable name (firstFavNumb) & store your favorite number.
+// 2. Create variable name (secondFavNumb) & store second fav Number.
+// 3. Check (firstFavNumb is greater then secondFavNumb)
+// 4. Check (firstFavNumb is less then secondFavNumb)
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+
+
+// 1. Create variable name (firstFavNumb) & store your favorite number.
+let firstFavNumb=7;
+// 2. Create variable name (secondFavNumb) & store second fav Number.
+let secondFavNumb=8;
+// 3. Check (firstFavNumb is greater then secondFavNumb)
+console.log(firstFavNumb>secondFavNumb);
+// 4. Check (firstFavNumb is less then secondFavNumb)
+console.log(firstFavNumb<secondFavNumb);
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
+console.log(firstFavNumb>=secondFavNumb);
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
+console.log(firstFavNumb<=secondFavNumb);
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
+console.log(firstFavNumb===secondFavNumb);
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
+console.log(firstFavNumb==secondFavNumb);
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+console.log(firstFavNumb!==secondFavNumb);
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+console.log(firstFavNumb!=secondFavNumb);
