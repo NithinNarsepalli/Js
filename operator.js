cd7511bde9b6233ca2e7a782da178803eeb9c8ae
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-17 20:49:16 +0530

    Update operator.js

--- operator.js
+++ operator.js
@@ -1,6 +1,20 @@
-//comparison operators
+//relational operators
 console.log(10>5);//greater than operator
 console.log(10<5);//lesser than operator
 console.log(10 ==10);//equals to operator
 console.log(10>=9);//greater than or equals to operator
 console.log(10<=5);//lesserthan or equals to operator
+-------------------------------------------------------------------
+//equality operators
+//strictly equality "==" it checks fo both the type and value
+console.log(10===10);//return true
+console.log(10==="10");//returns false
+//strictly not equality operator ""!=="" it checks both type and value
+console.log(10 !== "10");//true
+console.log(10 !== 10);//false
+//loosely equality operator "==" it checks for value only
+console.log(10=="10");//true
+//loosely not equlaity operator "!=" it checks for only value
+console.log(10!=="10");//true
+-------------------------------------------------------------------
+  
