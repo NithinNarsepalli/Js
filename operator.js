8f67e8f54d3616982a7cb97be8134424c751303e
Nithin Narsepalli <146744225+NithinNarsepalli@users.noreply.github.com>
2024-04-17 20:18:38 +0530

    Create operators.js

---
+++ operator.js
@@ -0,0 +1,6 @@
+//comparison operators
+console.log(10>5);//greater than operator
+console.log(10<5);//lesser than operator
+console.log(10 ==10);//equals to operator
+console.log(10>=9);//greater than or equals to operator
+console.log(10<=5);//lesserthan or equals to operator
